970a385856148d1d751af422d7287e6fa1676bc3
Björn Rundquist <50362568+lucidrunner@users.noreply.github.com>
2022-10-18 18:09:27 +0200

    Basic 1.0 version complete

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,13 +6,20 @@ namespace Calculator;
 
 internal class Program
 {
-    //TODO Check safer way to do this since these are currently editable in the program
-    private static readonly char[] SupportedOperators = new[] { '*', '+', '-', '/'};
+
+    //Slightly ugly design, but allows us to access the Operators array in switches
+    private const char PlusOperator = '+';
+    private const char MinusOperator = '-';
+    private const char MultiplicationOperator = '*';
+    private const char DivisionOperator = '/';
+
+    //As well as iterate over them without being able to change them
+    private static char[] SupportedOperators =>
+        new[] { PlusOperator, MinusOperator, MultiplicationOperator, DivisionOperator };
 
     static void Main(string[] args)
     {
         //Since we're not gonna do operations on our historical list we can just save it as a series of strings
-        //(if we were gonna do operations, since we do them on strings this would still work as mostly futureproof though)
         List<string> history = new List<string>();
 
         //Display our welcome message
@@ -21,12 +28,13 @@ internal class Program
         while (calculatorRunning)
         {
             bool correctInput = true;
+            ProcessedInput input;
             //Using a do-while since we always wanna run this a minimum of one time
             do
             {
                 //Input numbers - Basic version = we only deal with 2. I've decided to ask for it as a single string since it's easier to expand later
                 Console.Write("Input your two numbered operation (eg x*y): ");
-                correctInput = ReadInput(out ProcessedInput input);
+                correctInput = ReadInput(out input);
                 
                 //If we did not read the input in a correct format, go to the next cycle of the while
                 if(!correctInput)
@@ -35,14 +43,53 @@ internal class Program
                     continue;
                 }
 
-                //Otherwise, start handling the input
-                Console.WriteLine(input);
+                //There is one final validity check we have to do even if we have correct input - division by zero
+                //We could have done this in the reading of the input, but let's keep our responsibilities separate
+                if (input.OperatorSymbol == DivisionOperator && input.SecondInput.Equals(0))
+                {
+                    correctInput = false;
+                    Console.WriteLine("We can't divide by zero.");
+                }
 
             } while (!correctInput);
-            
-            calculatorRunning = PromptForRestart();
+
+            //Now that we have a correct expression split up, it's time to actually calculate the results
+            double answer = Calculate(input);
+
+            //And print it
+            string fullAnswer = $"{input} = {answer}";
+            Console.WriteLine(fullAnswer);
+
+            //Finally, we save it to our history list
+            history.Add(fullAnswer);
+
+            //After that, prompt to display the history list and if yes, do so
+            if (PromptForChoice("Display the Calculator history?"))
+            {
+                foreach (string calculation in history)
+                {
+                    Console.WriteLine(calculation);
+                }
+            }
+
+
+            calculatorRunning = PromptForChoice("Restart the Calculator?");
         }
         PrintSign("You are now leaving the Calculator. Goodbye!");
+    }
+    
+
+    private static double Calculate(ProcessedInput input)
+    {
+        //When we're dealing with two numbers this part is actually fairly straight forward
+        return input.OperatorSymbol switch
+        {
+            PlusOperator => input.FirstInput + input.SecondInput,
+            MinusOperator => input.SecondInput - input.SecondInput,
+            MultiplicationOperator => input.FirstInput * input.SecondInput,
+            DivisionOperator => input.FirstInput / input.SecondInput,
+            _ => double.NaN
+        };
     }
 
     private static bool ReadInput(out ProcessedInput processedInput)
@@ -50,7 +97,7 @@ internal class Program
         //One negative part about the pattern I did in this method
         //We have to assign a default record to the out argument since we might need to leave early
         //A risk of doing this is that we later miss assigning parts to the record
-        processedInput = new ProcessedInput(0, "*", 0);
+        processedInput = new ProcessedInput(0, '*', 0);
         
         //Read the input
         string input = Console.ReadLine() ?? "";
@@ -65,9 +112,9 @@ internal class Program
         
         //Two hickups from splitting occur - the first is that we eliminate the operator from the parts
         //We use a method to get around this
-        string mathOperator = GetOperator(input);
+        char? mathOperator = GetOperator(input);
         //If the operator was incorrect, return false
-        if (string.IsNullOrEmpty(mathOperator))
+        if (!mathOperator.HasValue)
             return false;
 
         //Second hickup is parsing from the parts to our doubles, which can fail for a number of reasons
@@ -82,37 +129,37 @@ internal class Program
             return false;
 
         //Before exiting, remember to create our correct record
-        processedInput = new ProcessedInput(firstNumber, mathOperator, secondNumber);
+        processedInput = new ProcessedInput(firstNumber, mathOperator.Value, secondNumber);
 
         //Finally, return true if we passed all our input checking
         return true;
     }
 
 
-    private static string GetOperator(string input)
+    private static char? GetOperator(string input)
     {
+        //Fairly straight forward since we're only dealing with one operator - go through our allowed operators and see which fits
         foreach (char supportedOperator in SupportedOperators)
         {
             if (input.Contains(supportedOperator))
-                return supportedOperator.ToString();
+                return supportedOperator;
         }
-
-        return "";
+        return null;
     }
-
-
+    
     /// <summary>
-    /// Prompts the Player on if they want to restart the calculator
+    /// Prompts the Player for yes / no on a selected text
     /// </summary>
-    /// <returns>True if the calculator should restart</returns>
-    private static bool PromptForRestart()
+    /// <param name="promptText">What will be printed, followed by [y/n]:</param>
+    /// <returns>True if the player answers y</returns>
+    private static bool PromptForChoice(string promptText)
     {
         //Nothing too fancy, we ask for the response and returns true if it's yes
         string playerResponse = "";
         //ToLower so we don't have to deal with Yy and Nn differences
         while (playerResponse.ToLower() != "y" && playerResponse.ToLower() != "n")
         {
-            Console.Write("Restart the Calculator? [y/n]: ");
+            Console.Write($"{promptText} [y/n]: ");
             //?? so we don't have to worry about null
             playerResponse = Console.ReadLine() ?? "";
         }
@@ -141,4 +188,11 @@ internal class Program
 }
 
 //Record for our input, split into parts and the operator
-internal record ProcessedInput (double FirstInput, string OperatorSymbol, double SecondInput) { }
+//Slightly breaking my Part 1 rule here by using a record, but prefer them over tuples
+internal record ProcessedInput(double FirstInput, char OperatorSymbol, double SecondInput)
+{
+    public override string ToString()
+    {
+        return $"{FirstInput}{OperatorSymbol}{SecondInput}";
+    }
+}
