b628d96223f2fc0ae951dab63d60a7503ce57e2d
Björn Rundquist <50362568+lucidrunner@users.noreply.github.com>
2022-10-18 10:11:38 +0200

    Initial Commit (Added pseudocode)

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,28 @@
 ﻿namespace Calculator
 {
+    // Välkomnande meddelande
+    // En lista för att spara historik för räkningar
+    // Användaren matar in tal och matematiska operation
+    //OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+    // Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
+    // Lägga resultat till listan
+    //Visa resultat
+    //Fråga användaren om den vill visa tidigare resultat.
+    //Visa tidigare resultat
+    //Fråga användaren om den vill avsluta eller fortsätta.
+
+    /*
+     *Checklista
+• Input/output
+• Lista med historik
+• Kontrollstrukturer
+• Välja lämpligt designmönster
+• Källkodshantering med GIT
+     *
+     * Fundera på designmönster - vad finns det för detta? Ett val kan ju vara att skriva hela miniräknaren i en första form och sen undersöka
+     * mönsterdesigner från det när vi kommer till det i kursen
+     */
+
     internal class Program
     {
         static void Main(string[] args)
